e274d4a1a4332a2df01b769ff1cf00f5714728c1
Kalinga Ray <mail.kalinga@gmail.com>
2018-05-14 22:55:58 +0200

    Refactor

--- url_check.py
+++ url_check.py
@@ -1,5 +1,4 @@
 import sys
-import time
 from pyspark.sql import SparkSession
 from pyspark.context import SparkContext
 import requests
@@ -33,41 +32,38 @@ def valid_urls(line):
 	    print "A IN valid URL"
             return (line,0)
 
+def readCSV():
+    global spark = SparkSession \
+        .builder \
+        .appName("GDELT_URL") \
+        .getOrCreate()
+
+    sc = SparkContext.getOrCreate()
+    # Without limit, with all records it gives java.lang.OutOfMemoryError: Java heap space spark
+    df = spark.read.csv('url.csv')#.limit(2000);  # <class 'pyspark.sql.dataframe.DataFrame'>
+    #df.show(df.count())
+    print "count", df.count
+
+def MR():
+    df_rdd = df.rdd  # <class 'pyspark.rdd.RDD'>
+    result_1 = df_rdd.flatMap(lambda x: x)  # <class 'pyspark.rdd.PipelinedRDD'>
+    print type(result_1)
+    result_2 = result_1.map(lambda x: str(x)).map(valid_urls).reduceByKey(lambda a, b: a + b).collect()
+    top100 = (sorted(result_2, key=lambda tup: tup[1], reverse=True))[0:100]
+    thefile = open('validURLCount100Domain.txt', 'w')
+    for item in top100:
+        print item[0]
+    print item[1]
+    contents = item[0] + ":" + str(item[1]) + "\n"
+    thefile.write("%s" % contents)
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
 	print sys.argv
         print("Usage: url.py")
         sys.exit(-1)
 
-    spark = SparkSession\
-        .builder\
-        .appName("GDELT_URL")\
-        .getOrCreate()
-    
-    sc = SparkContext.getOrCreate() 
-    # Without limit, with all records it gives java.lang.OutOfMemoryError: Java heap space spark
-    df = spark.read.csv('url.csv').limit(2000); #<class 'pyspark.sql.dataframe.DataFrame'>
-    df.show(df.count())
-    df_rdd=df.rdd #<class 'pyspark.rdd.RDD'>
-
-#    result=df.map(search_split).reduceByKey(lambda a, b: a + b).collect()
-#    top10=(sorted(result, key=lambda tup:tup[1], reverse=True))[0:10]
-#    thefile = open('top10domainCount.txt', 'w')
-#    for item in top10:
-#       contents=item[0]+":"+str(item[1])+"\n"
-#       thefile.write("%s" % contents)
-#    df.toDF().limit(200)
-#    result=df.flatMap(lambda row:row).map(valid_urls).reduceByKey(lambda a, b: a + b).collect()
-
-    result_1=df_rdd.flatMap(lambda x:x) #<class 'pyspark.rdd.PipelinedRDD'>
-    print type(result_1)
-    result_2=result_1.map(lambda x: str(x)).map(valid_urls).reduceByKey(lambda a, b: a + b).collect()
-    top100=(sorted(result_2, key=lambda tup:tup[1], reverse=True))[0:100]
-    thefile = open('validURLCount100Domain.txt', 'w')
-    for item in top100:
-	print item[0]
-	print item[1]
-        contents=item[0]+":"+str(item[1])+"\n"
-        thefile.write("%s" % contents)
+    readCSV()
+    MR()
 
     spark.stop()
