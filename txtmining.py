43ae0669e5f1f09afa85b7b32b56acce9724ed8a
kalinga bhusan ray <kara9147@dbblade13.prakinf.tu-ilmenau.de>
2018-06-03 23:42:37 +0200

    WIP

--- txtmining.py
+++ txtmining.py
@@ -38,31 +38,56 @@ def readJSON():
     #spark.conf.set("spark.executor.memory", "2g")
 
     dfReader = spark.read
-    df = dfReader.json('/data/wikipedia/articles.json').limit(10) #DataFrame[id: string, text: string, title: string, url: string]
-    #df.show(df.count())
+    df = dfReader.json('/data/wikipedia/articles.json').limit(1) #DataFrame[id: string, text: string, title: string, url: string]
+    #df.select("text").show(truncate = False)
+    #df.printSchema()
+#root                                                                            
+# |-- id: string (nullable = true)
+# |-- text: string (nullable = true)
+# |-- title: string (nullable = true)
+# |-- url: string (nullable = true)
+#
     #df.show()
-    print type( df)
+    #print type( df) <class 'pyspark.sql.dataframe.DataFrame'>
     
     df_stop = spark.read.text('/data/stopwords.txt')
     stopList = [s.value for s in df_stop.select('value').collect()]
+    stopList.extend(["</ref>", "<ref>", "<ref"])
 #    print df_stop;
 #    df_stop.show()
 #    print "count", df_stop.rdd.count()
 #    print df_stop.printSchema()
-
-    tokenizer = Tokenizer(inputCol="text",outputCol="words" )
+    print "###################################TOKENIZE###################################"
+    tokenizer = Tokenizer(inputCol="text",outputCol="words")
     wordsData = tokenizer.transform(df)
     wordsData.show(truncate = False)
 
+    print "###################################STOPWORD REMOVE#############################"
     remover = StopWordsRemover(inputCol="words", outputCol="filteredTxt", stopWords=stopList)
     wordsData_NoStop = remover.transform(wordsData)#.show(truncate=False)
-#    processedDF.select("filteredTxt")#.show(truncate = False)
+    wordsData_NoStop.select("filteredTxt").show(truncate = False)
+    wordsData_NoStop.printSchema()
 
     # fit a CountVectorizerModel from the corpus.
-    #cv = CountVectorizer(inputCol="words", outputCol="features", vocabSize=1<<20, minDF=2.0)
-    #model = cv.fit(df)
-    #result = model.transform(df)
+    #cv = CountVectorizer(inputCol="filteredTxt", outputCol="features", vocabSize=1<<20, minDF=2.0)
+    #model = cv.fit(wordsData_NoStop)
+    #result = model.transform(wordsData_NoStop)
     #result.show(truncate=False)
+
+    _filteredTxt = wordsData_NoStop.select("filteredTxt").rdd
+#    print type(_filteredTxt) #<class 'pyspark.rdd.RDD'>                                                       
+    # x:<class 'pyspark.sql.types.Row'>
+    # x["filteredTxt"]:<type 'list'> 
+
+    res = _filteredTxt.map(lambda x: x["filteredTxt"]).flatMap(lambda x: x).collect().map(lambda x: None if x=='' else x).reduceByKey(lambda a, b: a + b).collect()
+    print res
+    print "res", type(res)
+  #  result_1 = _filteredTxt.flatMap(lambda x: x).map(lambda x: str(x).split()).reduceByKey(lambda a, b: a + b).collect()
+  #  top100 = (sorted(result_2, key=lambda tup: tup[1], reverse=True))[0:100]
+  #  for item in top100:
+  #      print item[0]
+  #      print item[1]
+
     
     hashingTF = HashingTF(inputCol="filteredTxt", outputCol="rawFeatures", numFeatures=20)
     featurizedData = hashingTF.transform(wordsData_NoStop)
@@ -71,7 +96,12 @@ def readJSON():
     idfModel = idf.fit(featurizedData)
     rescaledData = idfModel.transform(featurizedData)
 
-    rescaledData.select("url", "features").show(truncate = False)
+#    rescaledData.select("url", "features").show(truncate = False)
+
+"""
+|https://en.wikipedia.org/wiki?curid=12 |(20,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],[72.22659218020823,70.94083035737691,63.523121308444104,73.40963526275935,65.72775185243236,74.98610589761486,61.38190288288243,66.17379088778677,61.754535892336285,58.32915995173227,38.889957285439024,72.42078091431766,2.53475825370568,65.94941911177912,68.19852531496224,61.27781832940704,59.27339450763014,66.46152500922966,101.72944950303025,52.1655046510415])                     |
+|https://en.wikipedia.org/wiki?curid=25 |(20,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],[32.478767361549735,43.931248157091204,26.845152002585714,30.495651479604525,26.854905384402386,26.886910042568214,28.781944892452046,21.95033063594878,16.621781623357805,26.015353030115335,16.654052296352713,29.447360332864573,1.3277305138458324,31.67136135209946,29.93725415324895,33.91385194952932,28.653489288048693,20.838292877003123,46.795546771393916,34.10265678018226])        |
+"""
 
     # TFIDF, short for term frequency-inverse document frequency
 #    result = processedDF.rdd.flatMap(lambda x: x["filteredTxt"]).flatMap(lambda x: x.split()).map(lambda x: (str(x),1)).reduceByKey(lambda a, b: a + b).collect()
