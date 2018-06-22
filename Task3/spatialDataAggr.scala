a67c626d909b50a8a9704536c3e93d7a09bd697b
kalinga bhusan ray <kara9147@dbblade13.prakinf.tu-ilmenau.de>
2018-06-23 00:06:08 +0200

    Task3 part 2 WIP

--- Task3/spatialDataAggr.scala
+++ Task3/spatialDataAggr.scala
@@ -9,8 +9,8 @@ import dbis.stark.spatial.SpatialRDD._
 import dbis.stark.spatial.partitioner.{SpatialGridPartitioner, SpatialPartitioner}
 import scala.collection.mutable.ListBuffer
 
-// For implicit conversions like converting RDDs to DataFrames
-//import org.apache.spark.implicits._
+import scala.collection.immutable.ListMap
+
 
 object spatialDataAggr {
 
@@ -30,58 +30,49 @@ object spatialDataAggr {
   }
 
   def world_level(spark: SparkSession) {
-	if (false) {
-            val df = spark.read.option("delimiter", ";").csv("/data/world_level2.csv")
-            df.printSchema()
-            val df_cols_id_name_ploy = df.select(col("_c0"),col("_c1"), col("_c2"), col("_c3"), col("_c4"),col("_c5"))
-            df_cols_id_name_ploy.show()
-   	}
-
 	val sc = spark.sparkContext
-	val df = sc.textFile("/data/world_level2.csv")
+	val world_level = sc.textFile("/data/world_level2.csv")
 	//df.foreach(println)
-	val df_clean = df.map(line => line.split(';')).filter((arr => arr.length==6))
+	val world_level_clean = world_level.map(line => line.split(';')).filter((arr => arr.length==6))
 
 //+---+---+-------+---+--------------------+--------------------+
 //|_c0|_c1|    _c2|_c3|                 _c4|                 _c5|
 //+---+---+-------+---+--------------------+--------------------+
 //|Cid| ??|????   |???|Country Name        |Country Map MPolygon|
-
-	val countries = df_clean.map(arr => (STObject(arr(5)), (arr(0).toInt,arr(4))))
+	val countries = world_level_clean.map(arr => (STObject(arr(5)), (arr(0).toInt,arr(4))))
 
 //	for (c <- countries) {
 //	    println(c._2)
 //       }
 
-//	System.exit(0)
 	val gdelt = sc.textFile("/data/gdelt/events_2013-04-01_TO_2015-10-18.tsv").map(line => line.split('\t')).filter(arr => arr(46).length > 0 )
 
 	val gdelt_event_points = gdelt.map(arr => (STObject("POINT ( " + arr(46) + " " + arr(47) + " )"), (arr(0).toInt, arr(51)) ))
 
 	// dimensions=2, lat, long
 	val gridPartitioner = new SpatialGridPartitioner(rdd=gdelt_event_points, partitionsPerDimension=10, pointsOnly=true, dimensions=2)
-	val gdelt_partitioned = gdelt_event_points.partitionBy(gridPartitioner)
+	val gdelt_partitioned = gdelt_event_points.partitionBy(gridPartitioner).liveIndex(order = 5)
 
 	val cids = df_clean.map(arr => arr(0).toInt).collect()
+
+	var count:Long = 0;
+	var country_event_count = Map("name"->count)
+
 	for (cid <- cids) {
-	    var country = countries.filter(arr => arr._2._1==cid).first()
-	    println(country._2._2,gdelt_partitioned.containedby(country._1).count())
+	    var country = countries.filter(arr => arr._2._1==cid).first() //.first() (STObject(arr(5))
+	//    count = gdelt_partitioned.containedby(country._1).count()
+	    country_event_count += (country._2._2 -> gdelt_partitioned.containedby(country._1).count())
 	}
-
 	
+	val sorted = ListMap(country_event_count.toSeq.sortWith(_._1 > _._1):_*)
+	sorted.drop(3).foreach(println)
 	
-
-	//val geom_having_line  = partionedCountries.intersects(STObject("LINESTRING ( 1.4143211 42.5378868, 14.3443989 55.1476253 )")).map(arr => (arr._2) )
-	//println(geom_having_line.collect().mkString("\n"))
-
-	println("------------------------------------------------------------------------")
-	println()
-        println()
-	println()
-	println("------------------------------------------------------------------------")
-
-  }
+  } // world_level
 
 
+} // spatialDataAggr
+//[Stage 30:=====================================================>(104 + 1) / 105]18/06/22 23:57:07 ERROR YarnScheduler: Lost executor 6 on dbblade14.prakinf.tu-ilmenau.de: Container marked as failed: container_1528188921975_0472_01_000007 on host: dbblade14.prakinf.tu-ilmenau.de. Exit status: 143. Diagnostics: Container killed on request. Exit code is 143
+//Container exited with a non-zero exit code 143
+//Killed by external signal
 
-}
+//Exception in thread "main" org.apache.spark.SparkException: Job aborted due to stage failure: ResultStage 31 (count at spatialDataAggr.scala:67) has failed the maximum allowable number of times: 4. Most recent failure reason: org.apache.spark.shuffle.MetadataFetchFailedException: Missing an output location for shuffle 0
