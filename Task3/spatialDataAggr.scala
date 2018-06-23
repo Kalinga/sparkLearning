f1a86d019e7b7c3f9252969b23ce194ac51a01db
kalinga bhusan ray <kara9147@dbblade13.prakinf.tu-ilmenau.de>
2018-06-23 12:39:19 +0200

    Task3 part 2 WIP cluster error

--- Task3/spatialDataAggr.scala
+++ Task3/spatialDataAggr.scala
@@ -29,39 +29,35 @@ object spatialDataAggr {
         world_level(spark)
   }
 
-  def world_level(spark: SparkSession) {
-	val sc = spark.sparkContext
-	val world_level = sc.textFile("/data/world_level2.csv")
-	//df.foreach(println)
-	val world_level_clean = world_level.map(line => line.split(';')).filter((arr => arr.length==6))
-
 //+---+---+-------+---+--------------------+--------------------+
 //|_c0|_c1|    _c2|_c3|                 _c4|                 _c5|
 //+---+---+-------+---+--------------------+--------------------+
 //|Cid| ??|????   |???|Country Name        |Country Map MPolygon|
+
+  def world_level(spark: SparkSession) {
+	val sc = spark.sparkContext
+
+	val world_level = sc.textFile("/data/world_level2.csv")
+	//df.foreach(println)
+	val world_level_clean = world_level.map(line => line.split(';')).filter((arr => arr.length==6))
 	val countries = world_level_clean.map(arr => (STObject(arr(5)), (arr(0).toInt,arr(4))))
-
-//	for (c <- countries) {
-//	    println(c._2)
-//       }
-
+	
 	val gdelt = sc.textFile("/data/gdelt/events_2013-04-01_TO_2015-10-18.tsv").map(line => line.split('\t')).filter(arr => arr(46).length > 0 )
-
 	val gdelt_event_points = gdelt.map(arr => (STObject("POINT ( " + arr(46) + " " + arr(47) + " )"), (arr(0).toInt, arr(51)) ))
 
 	// dimensions=2, lat, long
 	val gridPartitioner = new SpatialGridPartitioner(rdd=gdelt_event_points, partitionsPerDimension=10, pointsOnly=true, dimensions=2)
 	val gdelt_partitioned = gdelt_event_points.partitionBy(gridPartitioner).liveIndex(order = 5)
 
-	val cids = df_clean.map(arr => arr(0).toInt).collect()
+	val cids = world_level_clean.map(arr => arr(0).toInt).collect()
 
 	var count:Long = 0;
 	var country_event_count = Map("name"->count)
 
 	for (cid <- cids) {
-	    var country = countries.filter(arr => arr._2._1==cid).first() //.first() (STObject(arr(5))
-	//    count = gdelt_partitioned.containedby(country._1).count()
-	    country_event_count += (country._2._2 -> gdelt_partitioned.containedby(country._1).count())
+	    var country = countries.filter(arr => arr._2._1 == cid).first() //.first() (STObject(arr(5))
+	    count = gdelt_partitioned.containedby(country._1).count()
+	    country_event_count += (country._2._2 -> count)
 	}
 	
 	val sorted = ListMap(country_event_count.toSeq.sortWith(_._1 > _._1):_*)
@@ -71,6 +67,9 @@ object spatialDataAggr {
 
 
 } // spatialDataAggr
+
+//[Error]
+
 //[Stage 30:=====================================================>(104 + 1) / 105]18/06/22 23:57:07 ERROR YarnScheduler: Lost executor 6 on dbblade14.prakinf.tu-ilmenau.de: Container marked as failed: container_1528188921975_0472_01_000007 on host: dbblade14.prakinf.tu-ilmenau.de. Exit status: 143. Diagnostics: Container killed on request. Exit code is 143
 //Container exited with a non-zero exit code 143
 //Killed by external signal
